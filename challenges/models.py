97a53bc43705edffcbddf39b896196e12479e8f5
JeppeAug <jeppe.aug@gmail.com>
2019-12-06 07:34:22 +0100

    migrated models

--- challenges/models.py
+++ challenges/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Challenge(models.Model):
+    image = models.ImageField(upload_to='images/')
+    headline = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+    pass
