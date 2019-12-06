c1eceb0fd6ebb2cb65cd34c1aba38eb8f8839eb4
JeppeAug <jeppe.aug@gmail.com>
2019-12-06 06:27:29 +0100

    created django project

---
+++ chavolve/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for chavolve project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chavolve.settings')
+
+application = get_wsgi_application()
